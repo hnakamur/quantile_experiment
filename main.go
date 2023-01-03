a2a562160fed1aa1cc9eff4abcc5633a4d725dd3
Hiroaki Nakamura <hnakamur@gmail.com>
2023-01-04 03:35:09 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Summary struct {
+	nrElems    uint64
+	epsilon    float64
+	alloced    uint64
+	maxAlloced uint64
+	head       tuple
+	freelist   *tuple
+}
+
+type tuple struct {
+	value uint64
+	g     float64
+	delta uint64
+	prev  *tuple
+	next  *tuple
+}
+
+func (t *tuple) listEmpty() bool {
+	return t.next == t
+}
+
+func (t *tuple) listInit() {
+	t.next = t
+	t.prev = t
+}
+
+func (t *tuple) listDel() {
+	t.next.prev = t.prev
+	t.prev.next = t.next
+}
+
+func (t *tuple) listAddHeadTo(l *tuple) {
+	t.next = l.next
+	t.next.prev = t
+	l.next = t
+	t.prev = l
+}
+
+func (t *tuple) listAddTailTo(l *tuple) {
+	t.listAddHeadTo(l.prev)
+}
+
+var ullog2Table = []uint8{
+	0, 58, 1, 59, 47, 53, 2, 60, 39, 48, 27, 54, 33, 42, 3, 61,
+	51, 37, 40, 49, 18, 28, 20, 55, 30, 34, 11, 43, 14, 22, 4, 62,
+	57, 46, 52, 38, 26, 32, 41, 50, 36, 17, 19, 29, 10, 13, 21, 56,
+	45, 25, 31, 35, 16, 9, 12, 44, 24, 15, 8, 23, 7, 6, 5, 63,
+}
+
+// Ported from https://stackoverflow.com/a/23000588/1391518
+func ullog2(n uint64) uint64 {
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	n |= n >> 32
+	return uint64(ullog2Table[(n*0x03f6eaf2cd271461)>>58])
+}
+
+func NewSummary(epsilon float64) *Summary {
+	s := &Summary{}
+	s.Init(epsilon)
+	return s
+}
+
+func (s *Summary) Init(epsilon float64) {
+	s.head.listInit()
+	s.epsilon = epsilon
+}
+
+func (s *Summary) sanityCheck() error {
+	nrElems := uint64(0)
+	nrAlloced := uint64(0)
+	cur := s.head.next
+	for cur != &s.head {
+		nrElems += uint64(cur.g)
+		nrAlloced++
+		if float64(s.nrElems) > (1 / s.epsilon) {
+			// there must be enough observations for this to become true
+			if cur.g+float64(cur.delta) > float64(s.nrElems)*s.epsilon*2 {
+				return errors.New("error: g+delta > s.nrElems*s.epsilon*2")
+			}
+		}
+		if nrAlloced > s.alloced {
+			return errors.New("error: nrAlloced >s.alloced")
+		}
+		cur = cur.next
+	}
+	if nrElems != s.nrElems {
+		return errors.New("error: nrElems != s.nrElems")
+	}
+	if nrAlloced != s.alloced {
+		return errors.New("error: nrAlloced != s.alloced")
+	}
+	return nil
+}
+
+func (s *Summary) allocTuple() *tuple {
+	s.alloced++
+	if s.alloced > s.maxAlloced {
+		s.maxAlloced = s.alloced
+	}
+
+	if s.freelist != nil {
+		ret := s.freelist
+		s.freelist = s.freelist.next
+		return ret
+	}
+	return &tuple{}
+}
+
+func (s *Summary) freeTuple(t *tuple) {
+	s.alloced--
+
+	t.next = s.freelist
+	s.freelist = t
+}
+
+func (s *Summary) Query(q float64) uint64 {
+	if s.head.listEmpty() {
+		return 0
+	}
+
+	rank := int(0.5 + q*float64(s.nrElems))
+	ne := float64(s.nrElems) * s.epsilon
+	gi := float64(0)
+	cur := s.head.next
+	for {
+		next := cur.next
+		if next == &s.head {
+			return cur.value
+		}
+
+		gi += cur.g
+		rankPlusNe := float64(rank) + ne
+		giPlusNextG := gi + next.g
+		if rankPlusNe < giPlusNextG {
+			return cur.value
+		}
+		if rankPlusNe < giPlusNextG+float64(next.delta) {
+			return next.value
+		}
+		cur = next
+	}
+}
+
+func (s *Summary) band(delta float64) uint64 {
+	diff := uint64(1 + s.epsilon*float64(s.nrElems)*2 - delta)
+	if diff == 1 {
+		return 0
+	}
+	const ullog2_2 = 1
+	return ullog2(diff) / ullog2_2
+}
+
+func (s *Summary) compress() {
+	if s.nrElems < 2 {
+		return
+	}
+
+	maxCompress := int(2 * s.epsilon * float64(s.nrElems))
+	prev := s.head.prev
+	cur := prev.prev
+	for cur != &s.head {
+		bPlus1 := s.band(float64(prev.delta))
+		bi := s.band(float64(cur.delta))
+		// fmt.Printf("compress#1 prev.value=%d, cur.value=%d, bi=%d, b_plus_1=%d, lhs=%g, max_compress=%d, del=%v\n", prev.value, cur.value, bi, bPlus1, cur.g+prev.g+float64(prev.delta), maxCompress, bi <= bPlus1 && cur.g+prev.g+float64(prev.delta) <= float64(maxCompress))
+		if bi <= bPlus1 && cur.g+prev.g+float64(prev.delta) <= float64(maxCompress) {
+			// fmt.Printf("compress prev.value=%d, g=%g, cur.value=%d,g=%g\n", prev.value, prev.g, cur.value, cur.g)
+			prev.g += cur.g
+			cur.listDel()
+			s.freeTuple(cur)
+			cur = prev.prev
+			continue
+		}
+		prev = cur
+		cur = cur.prev
+	}
+}
+
+func (s *Summary) InsertValue(value uint64) {
+	new := s.allocTuple()
+	new.delta = 0
+	new.value = value
+	new.g = 1
+	new.listInit()
+
+	s.nrElems++
+
+	// first insert
+	if s.head.listEmpty() {
+		// fmt.Printf("first insert, value=%d\n", new.value)
+		new.listAddHeadTo(&s.head)
+		return
+	}
+
+	cur := s.head.next
+	// v < v0, new min
+	if cur.value > new.value {
+		// fmt.Printf("new min, value=%d\n", new.value)
+		new.listAddHeadTo(&s.head)
+	} else {
+		gi := float64(0)
+		for cur.next != &s.head {
+			next := cur.next
+			gi += cur.g
+			if cur.value <= new.value && new.value < next.value {
+				// INSERT "(v, 1, Δ)" into S between vi and vi+1
+				new.delta = uint64(cur.g) + cur.delta - 1
+				// fmt.Printf("insert, value=%d, delta=%d\n", new.value, new.delta)
+				new.listAddHeadTo(cur)
+				goto out
+			}
+			cur = next
+		}
+		// v > vs-1, new max
+		new.listAddTailTo(&s.head)
+		// fmt.Printf("new max, value=%d\n", new.value)
+	}
+
+out:
+	if s.nrElems%uint64(1/(2*s.epsilon)) != 0 {
+		// fmt.Printf("calling compress, nrElems=%d, rhs=%d\n", s.nrElems, uint64(1/(2*s.epsilon)))
+		s.compress()
+	}
+}
+
+func (s *Summary) String() string {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "nr_elems: %d, epsilon: %.02f, alloced: %d, overfilled: %.02f, max_alloced: %d\n", s.nrElems, s.epsilon, s.alloced, 2*s.epsilon*float64(s.nrElems),
+		s.maxAlloced)
+	if s.head.listEmpty() {
+		b.WriteString("Empty summary\n")
+		return s.String()
+	}
+
+	cur := s.head.next
+	for cur != &s.head {
+		if cur != s.head.next {
+			b.WriteByte(' ')
+		}
+		fmt.Fprintf(&b, "(v: %d, g: %.02f, d: %d)", cur.value, cur.g, cur.delta)
+		cur = cur.next
+	}
+	b.WriteByte('\n')
+	return b.String()
+}
+
+func (s *Summary) Combine(s2 *Summary) (*Summary, error) {
+	if s.epsilon != s2.epsilon {
+		return nil, errors.New("epsilon must be equal")
+	}
+
+	snew := NewSummary(s.epsilon)
+	cur1 := s.head.next
+	cur2 := s2.head.next
+	for cur1 != &s.head && cur2 != &s2.head {
+		tnew := snew.allocTuple()
+		if cur1.value < cur2.value {
+			tnew.value = cur1.value
+			tnew.g = cur1.g
+			tnew.delta = cur1.delta
+			cur1 = cur1.next
+		} else {
+			tnew.value = cur2.value
+			tnew.g = cur2.g
+			tnew.delta = cur2.delta
+			cur2 = cur2.next
+		}
+		tnew.listAddTailTo(&snew.head)
+		snew.nrElems += uint64(tnew.g)
+	}
+	for cur1 != &s.head {
+		tnew := snew.allocTuple()
+		tnew.value = cur1.value
+		tnew.g = cur1.g
+		tnew.delta = cur1.delta
+		tnew.listAddTailTo(&snew.head)
+		snew.nrElems += uint64(tnew.g)
+		cur1 = cur1.next
+	}
+	for cur2 != &s2.head {
+		tnew := snew.allocTuple()
+		tnew.value = cur2.value
+		tnew.g = cur2.g
+		tnew.delta = cur2.delta
+		tnew.listAddTailTo(&snew.head)
+		snew.nrElems += uint64(tnew.g)
+		cur2 = cur2.next
+	}
+	snew.maxAlloced = snew.alloced
+	snew.compress()
+	return snew, nil
+}
---
+++ main_test.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestSummaryInsertValue(t *testing.T) {
+	testCases := []struct {
+		input []float64
+		want  string
+	}{
+		{
+			input: []float64{12, 6, 10, 1},
+			want: `nr_elems: 4, epsilon: 0.01, alloced: 4, overfilled: 0.08, max_alloced: 4
+(v: 1, g: 1.00, d: 0) (v: 6, g: 1.00, d: 0) (v: 10, g: 1.00, d: 0) (v: 12, g: 1.00, d: 0)
+queried: 0.00, found: 1
+queried: 0.25, found: 1
+queried: 0.50, found: 6
+queried: 0.75, found: 10
+queried: 1.00, found: 12
+`,
+		},
+		{
+			input: []float64{
+				3658, 3673, 3693, 3715, 3723, 3724, 3724, 3690, 3695, 3689, 3695, 3700,
+				3690, 3699, 3699, 3701, 3704, 3704, 3714, 3707, 3698, 3701, 3697, 3697,
+				3712, 3713, 3714, 3715, 3717, 3712, 3712, 3717, 3728, 3728, 3744, 3751,
+				3764, 3751, 3798, 3802, 3800, 3824, 3810, 3824, 3811, 3802, 3811, 3801,
+				3791, 3796, 3803, 3817, 3819, 3818, 3815, 3804, 3796, 3784, 3783, 3784,
+				3774, 3776, 3776, 3764, 3763, 3806, 3819, 3835, 3825, 3786, 3795, 3795,
+				3776, 3760, 3789, 3786, 3771, 3778, 3782, 3776, 3781, 3784, 3801, 3810,
+				3815, 3792, 3764, 3770, 3746, 3741, 3746, 3756, 3755, 3775, 3776, 3773,
+				3777, 3801, 3804, 3807,
+			},
+			want: `nr_elems: 100, epsilon: 0.01, alloced: 100, overfilled: 2.00, max_alloced: 100
+(v: 3658, g: 1.00, d: 0) (v: 3673, g: 1.00, d: 0) (v: 3689, g: 1.00, d: 0) (v: 3690, g: 1.00, d: 0) (v: 3690, g: 1.00, d: 0) (v: 3693, g: 1.00, d: 0) (v: 3695, g: 1.00, d: 0) (v: 3695, g: 1.00, d: 0) (v: 3697, g: 1.00, d: 0) (v: 3697, g: 1.00, d: 0) (v: 3698, g: 1.00, d: 0) (v: 3699, g: 1.00, d: 0) (v: 3699, g: 1.00, d: 0) (v: 3700, g: 1.00, d: 0) (v: 3701, g: 1.00, d: 0) (v: 3701, g: 1.00, d: 0) (v: 3704, g: 1.00, d: 0) (v: 3704, g: 1.00, d: 0) (v: 3707, g: 1.00, d: 0) (v: 3712, g: 1.00, d: 0) (v: 3712, g: 1.00, d: 0) (v: 3712, g: 1.00, d: 0) (v: 3713, g: 1.00, d: 0) (v: 3714, g: 1.00, d: 0) (v: 3714, g: 1.00, d: 0) (v: 3715, g: 1.00, d: 0) (v: 3715, g: 1.00, d: 0) (v: 3717, g: 1.00, d: 0) (v: 3717, g: 1.00, d: 0) (v: 3723, g: 1.00, d: 0) (v: 3724, g: 1.00, d: 0) (v: 3724, g: 1.00, d: 0) (v: 3728, g: 1.00, d: 0) (v: 3728, g: 1.00, d: 0) (v: 3741, g: 1.00, d: 0) (v: 3744, g: 1.00, d: 0) (v: 3746, g: 1.00, d: 0) (v: 3746, g: 1.00, d: 0) (v: 3751, g: 1.00, d: 0) (v: 3751, g: 1.00, d: 0) (v: 3755, g: 1.00, d: 0) (v: 3756, g: 1.00, d: 0) (v: 3760, g: 1.00, d: 0) (v: 3763, g: 1.00, d: 0) (v: 3764, g: 1.00, d: 0) (v: 3764, g: 1.00, d: 0) (v: 3764, g: 1.00, d: 0) (v: 3770, g: 1.00, d: 0) (v: 3771, g: 1.00, d: 0) (v: 3773, g: 1.00, d: 0) (v: 3774, g: 1.00, d: 0) (v: 3775, g: 1.00, d: 0) (v: 3776, g: 1.00, d: 0) (v: 3776, g: 1.00, d: 0) (v: 3776, g: 1.00, d: 0) (v: 3776, g: 1.00, d: 0) (v: 3776, g: 1.00, d: 0) (v: 3777, g: 1.00, d: 0) (v: 3778, g: 1.00, d: 0) (v: 3781, g: 1.00, d: 0) (v: 3782, g: 1.00, d: 0) (v: 3783, g: 1.00, d: 0) (v: 3784, g: 1.00, d: 0) (v: 3784, g: 1.00, d: 0) (v: 3784, g: 1.00, d: 0) (v: 3786, g: 1.00, d: 0) (v: 3786, g: 1.00, d: 0) (v: 3789, g: 1.00, d: 0) (v: 3791, g: 1.00, d: 0) (v: 3792, g: 1.00, d: 0) (v: 3795, g: 1.00, d: 0) (v: 3795, g: 1.00, d: 0) (v: 3796, g: 1.00, d: 0) (v: 3796, g: 1.00, d: 0) (v: 3798, g: 1.00, d: 0) (v: 3800, g: 1.00, d: 0) (v: 3801, g: 1.00, d: 0) (v: 3801, g: 1.00, d: 0) (v: 3801, g: 1.00, d: 0) (v: 3802, g: 1.00, d: 0) (v: 3802, g: 1.00, d: 0) (v: 3803, g: 1.00, d: 0) (v: 3804, g: 1.00, d: 0) (v: 3804, g: 1.00, d: 0) (v: 3806, g: 1.00, d: 0) (v: 3807, g: 1.00, d: 0) (v: 3810, g: 1.00, d: 0) (v: 3810, g: 1.00, d: 0) (v: 3811, g: 1.00, d: 0) (v: 3811, g: 1.00, d: 0) (v: 3815, g: 1.00, d: 0) (v: 3815, g: 1.00, d: 0) (v: 3817, g: 1.00, d: 0) (v: 3818, g: 1.00, d: 0) (v: 3819, g: 1.00, d: 0) (v: 3819, g: 1.00, d: 0) (v: 3824, g: 1.00, d: 0) (v: 3824, g: 1.00, d: 0) (v: 3825, g: 1.00, d: 0) (v: 3835, g: 1.00, d: 0)
+queried: 0.00, found: 3658
+queried: 0.25, found: 3715
+queried: 0.50, found: 3774
+queried: 0.75, found: 3800
+queried: 1.00, found: 3835
+`,
+		},
+	}
+	for i, tc := range testCases {
+		summary := NewSummary(0.01)
+		for _, v := range tc.input {
+			summary.InsertValue(uint64(v))
+		}
+		var b strings.Builder
+		b.WriteString(summary.String())
+		b.WriteString(query(summary, 0))
+		b.WriteString(query(summary, .25))
+		b.WriteString(query(summary, .5))
+		b.WriteString(query(summary, .75))
+		b.WriteString(query(summary, 1))
+		got := b.String()
+		want := tc.want
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("case %d result mismatch (-want +got):\n%s", i, diff)
+			fmt.Print(got)
+		}
+
+		if err := summary.sanityCheck(); err != nil {
+			t.Fatalf("sanity check failed: %s", err)
+		}
+	}
+}
+
+//go:embed combine_test.dat
+var combineTestData []byte
+
+func readCombineTestData() ([]uint64, error) {
+	var values []uint64
+	scanner := bufio.NewScanner(bytes.NewReader(combineTestData))
+	for scanner.Scan() {
+		v, err := strconv.ParseUint(scanner.Text(), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		values = append(values, v)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return values, nil
+}
+
+func TestSummaryCombine(t *testing.T) {
+	s1Inputs, err := readCombineTestData()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s1 := NewSummary(0.01)
+	for _, v := range s1Inputs {
+		s1.InsertValue(v)
+	}
+
+	s2 := NewSummary(0.01)
+	for i := 0; i < 1000; i++ {
+		s2.InsertValue(111)
+	}
+
+	snew, err := s1.Combine(s2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b strings.Builder
+	// b.WriteString(s1.String())
+	// b.WriteString(query(s1, .02))
+	// b.WriteString(query(s1, .1))
+	// b.WriteString(query(s1, .25))
+	// b.WriteString(query(s1, .5))
+	// b.WriteString(query(s1, .75))
+	// b.WriteString(query(s1, .82))
+	// b.WriteString(query(s1, .88))
+	// b.WriteString(query(s1, .86))
+	// b.WriteString(query(s1, .99))
+
+	b.WriteString(snew.String())
+	b.WriteString(query(snew, .02))
+	b.WriteString(query(snew, .1))
+	b.WriteString(query(snew, .25))
+	b.WriteString(query(snew, .5))
+	b.WriteString(query(snew, .75))
+	b.WriteString(query(snew, .82))
+	b.WriteString(query(snew, .88))
+	b.WriteString(query(snew, .86))
+	b.WriteString(query(snew, .99))
+	got := b.String()
+	// 	want := `nr_elems: 10000, epsilon: 0.01, alloced: 71, overfilled: 200.00, max_alloced: 101
+	// (v: 124, g: 151.00, d: 1) (v: 294, g: 172.00, d: 12) (v: 364, g: 72.00, d: 0) (v: 540, g: 156.00, d: 0) (v: 709, g: 158.00, d: 1) (v: 867, g: 121.00, d: 53) (v: 925, g: 102.00, d: 6) (v: 1087, g: 181.00, d: 0) (v: 1222, g: 147.00, d: 5) (v: 1393, g: 149.00, d: 0) (v: 1500, g: 106.00, d: 1) (v: 1705, g: 183.00, d: 4) (v: 1873, g: 171.00, d: 0) (v: 2036, g: 163.00, d: 4) (v: 2227, g: 183.00, d: 1) (v: 2362, g: 148.00, d: 0) (v: 2438, g: 50.00, d: 72) (v: 2567, g: 160.00, d: 0) (v: 2754, g: 178.00, d: 0) (v: 2921, g: 153.00, d: 1) (v: 3069, g: 155.00, d: 0) (v: 3181, g: 104.00, d: 13) (v: 3324, g: 149.00, d: 2) (v: 3426, g: 95.00, d: 0) (v: 3584, g: 160.00, d: 0) (v: 3784, g: 176.00, d: 0) (v: 3926, g: 153.00, d: 0) (v: 4067, g: 141.00, d: 0) (v: 4218, g: 99.00, d: 53) (v: 4370, g: 194.00, d: 0) (v: 4567, g: 185.00, d: 1) (v: 4762, g: 173.00, d: 12) (v: 4919, g: 183.00, d: 0) (v: 5115, g: 189.00, d: 10) (v: 5198, g: 97.00, d: 0) (v: 5368, g: 191.00, d: 0) (v: 5499, g: 128.00, d: 1) (v: 5710, g: 168.00, d: 30) (v: 5927, g: 134.00, d: 46) (v: 5928, g: 115.00, d: 2) (v: 6091, g: 163.00, d: 0) (v: 6226, g: 152.00, d: 0) (v: 6353, g: 123.00, d: 17) (v: 6429, g: 93.00, d: 0) (v: 6571, g: 136.00, d: 24) (v: 6649, g: 104.00, d: 0) (v: 6799, g: 149.00, d: 17) (v: 6862, g: 90.00, d: 0) (v: 6996, g: 139.00, d: 0) (v: 7178, g: 165.00, d: 0) (v: 7343, g: 171.00, d: 1) (v: 7488, g: 142.00, d: 1) (v: 7647, g: 168.00, d: 3) (v: 7763, g: 144.00, d: 0) (v: 7856, g: 79.00, d: 1) (v: 8042, g: 173.00, d: 0) (v: 8206, g: 159.00, d: 2) (v: 8335, g: 115.00, d: 0) (v: 8440, g: 80.00, d: 36) (v: 8537, g: 133.00, d: 0) (v: 8690, g: 140.00, d: 0) (v: 8858, g: 169.00, d: 1) (v: 8980, g: 129.00, d: 0) (v: 9170, g: 167.00, d: 17) (v: 9224, g: 77.00, d: 4) (v: 9355, g: 128.00, d: 2) (v: 9430, g: 1.00, d: 129) (v: 9497, g: 155.00, d: 1) (v: 9718, g: 180.00, d: 8) (v: 9802, g: 86.00, d: 0) (v: 9999, g: 197.00, d: 0)
+	// queried: 0.02, found: 124
+	// queried: 0.10, found: 925
+	// queried: 0.25, found: 2567
+	// queried: 0.50, found: 5115
+	// queried: 0.75, found: 7488
+	// queried: 0.82, found: 8206
+	// queried: 0.88, found: 8858
+	// queried: 0.86, found: 8537
+	// queried: 0.99, found: 9999
+	// `
+
+	want := `nr_elems: 11000, epsilon: 0.01, alloced: 71, overfilled: 220.00, max_alloced: 143
+(v: 111, g: 93.00, d: 0) (v: 111, g: 206.00, d: 0) (v: 111, g: 219.00, d: 0) (v: 111, g: 216.00, d: 0) (v: 111, g: 212.00, d: 0) (v: 124, g: 205.00, d: 1) (v: 294, g: 172.00, d: 12) (v: 364, g: 72.00, d: 0) (v: 540, g: 156.00, d: 0) (v: 709, g: 158.00, d: 1) (v: 867, g: 121.00, d: 53) (v: 925, g: 102.00, d: 6) (v: 1087, g: 181.00, d: 0) (v: 1222, g: 147.00, d: 5) (v: 1393, g: 149.00, d: 0) (v: 1500, g: 106.00, d: 1) (v: 1705, g: 183.00, d: 4) (v: 1873, g: 171.00, d: 0) (v: 2036, g: 163.00, d: 4) (v: 2227, g: 183.00, d: 1) (v: 2362, g: 148.00, d: 0) (v: 2567, g: 210.00, d: 0) (v: 2754, g: 178.00, d: 0) (v: 2921, g: 153.00, d: 1) (v: 3069, g: 155.00, d: 0) (v: 3181, g: 104.00, d: 13) (v: 3324, g: 149.00, d: 2) (v: 3426, g: 95.00, d: 0) (v: 3584, g: 160.00, d: 0) (v: 3784, g: 176.00, d: 0) (v: 3926, g: 153.00, d: 0) (v: 4067, g: 141.00, d: 0) (v: 4218, g: 99.00, d: 53) (v: 4370, g: 194.00, d: 0) (v: 4567, g: 185.00, d: 1) (v: 4762, g: 173.00, d: 12) (v: 4919, g: 183.00, d: 0) (v: 5115, g: 189.00, d: 10) (v: 5198, g: 97.00, d: 0) (v: 5368, g: 191.00, d: 0) (v: 5499, g: 128.00, d: 1) (v: 5710, g: 168.00, d: 30) (v: 5927, g: 134.00, d: 46) (v: 5928, g: 115.00, d: 2) (v: 6091, g: 163.00, d: 0) (v: 6226, g: 152.00, d: 0) (v: 6429, g: 216.00, d: 0) (v: 6571, g: 136.00, d: 24) (v: 6649, g: 104.00, d: 0) (v: 6799, g: 149.00, d: 17) (v: 6862, g: 90.00, d: 0) (v: 6996, g: 139.00, d: 0) (v: 7178, g: 165.00, d: 0) (v: 7343, g: 171.00, d: 1) (v: 7488, g: 142.00, d: 1) (v: 7647, g: 168.00, d: 3) (v: 7763, g: 144.00, d: 0) (v: 7856, g: 79.00, d: 1) (v: 8042, g: 173.00, d: 0) (v: 8206, g: 159.00, d: 2) (v: 8335, g: 115.00, d: 0) (v: 8537, g: 213.00, d: 0) (v: 8690, g: 140.00, d: 0) (v: 8858, g: 169.00, d: 1) (v: 8980, g: 129.00, d: 0) (v: 9170, g: 167.00, d: 17) (v: 9355, g: 205.00, d: 2) (v: 9497, g: 156.00, d: 1) (v: 9718, g: 180.00, d: 8) (v: 9802, g: 86.00, d: 0) (v: 9999, g: 197.00, d: 0)
+queried: 0.02, found: 111
+queried: 0.10, found: 124
+queried: 0.25, found: 1705
+queried: 0.50, found: 4567
+queried: 0.75, found: 7178
+queried: 0.82, found: 8042
+queried: 0.88, found: 8690
+queried: 0.86, found: 8335
+queried: 0.99, found: 9999
+`
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("result mismatch (-want +got):\n%s", diff)
+		fmt.Print(got)
+	}
+
+	// if err := s1.sanityCheck(); err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	if err := snew.sanityCheck(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func query(s *Summary, q float64) string {
+	v := s.Query(q)
+	return fmt.Sprintf("queried: %.02f, found: %d\n", q, v)
+}
