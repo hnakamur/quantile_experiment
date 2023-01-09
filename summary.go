8532c51f214f4cd9ad2a1af05bd69142291c6287
Hiroaki Nakamura <hnakamur@gmail.com>
2023-01-09 14:57:15 +0900

    Delete debug logs

--- summary.go
+++ summary.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"log"
 	"math"
 	"sort"
 )
@@ -43,7 +42,6 @@ func (s *Summary) Add(v float64) {
 		s.tuples = append(s.tuples, t)
 	}
 	s.n++
-	log.Printf("Add v=%g, n=%d, tn=%d, i=%d, t=%+v", v, s.n, len(s.tuples), i, t)
 
 	if s.n%s.compressingInterval == 0 {
 		s.compress()
@@ -59,8 +57,6 @@ func (s *Summary) Quantile(p float64) (float64, error) {
 	margin := int(math.Ceil(s.epsilon * float64(s.n)))
 	rankMinusMargin := int(rank) - margin
 	rankPlusMargin := int(rank) + margin
-	log.Printf("Quantile, p=%g, n=%d, tn=%d, rank=%g, margin=%d, rank-margin=%d, rank+margin=%d",
-		p, s.n, len(s.tuples), rank, margin, rankMinusMargin, rankPlusMargin)
 	bestIndex := -1
 	bestDist := math.MaxFloat64
 	rMin := 0
@@ -68,7 +64,6 @@ func (s *Summary) Quantile(p float64) (float64, error) {
 		t := &s.tuples[i]
 		rMin += t.gap
 		rMax := rMin + t.delta
-		// log.Printf("Quantile, i=%d, rMin=%d, rMax=%d", i, rMin, rMax)
 		if rankMinusMargin <= rMin && rMax <= rankPlusMargin {
 			currentDist := math.Abs(rank - float64(rMin+rMax)/2)
 			if currentDist < bestDist {
@@ -89,7 +84,6 @@ func (s *Summary) compress() {
 		for i < len(s.tuples)-1 && s.deleteIfNeeded(i, threshold) {
 		}
 	}
-	log.Printf("compress n=%d, tn=%d", s.n, len(s.tuples))
 }
 
 func (s *Summary) deleteIfNeeded(i, threshold int) bool {
